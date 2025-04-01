57ad18eb6c46c7682c9923eab2c72199071c0029
dmytrove <info@dmytrove.com>
2025-04-01 18:17:12 +0300

    Refactor: Switch to core DataTables JS/CSS

--- js/table.js
+++ js/table.js
@@ -86,25 +86,6 @@ function initializeDataTable() {
   } else {
       console.error("Table body not found for event delegation.");
   }
-
-  // Style the search input using Bootstrap classes
-  const searchInput = document.querySelector('.dt-search input');
-  if (searchInput) {
-    searchInput.classList.add('form-control', 'form-control-sm');
-    const searchLabel = document.querySelector('.dt-search label');
-    if(searchLabel) {
-        // Wrap input in an input-group for better styling
-        const inputGroup = document.createElement('div');
-        inputGroup.classList.add('input-group', 'input-group-sm', 'mb-2');
-        searchLabel.parentNode.insertBefore(inputGroup, searchLabel);
-        inputGroup.appendChild(searchLabel);
-        inputGroup.appendChild(searchInput);
-        searchLabel.classList.add('input-group-text');
-        searchLabel.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
-          <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
-        </svg>`;
-    }
-  }
 }
 
 // Update the table display using DataTables API
